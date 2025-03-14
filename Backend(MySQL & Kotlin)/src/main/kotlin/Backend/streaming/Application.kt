6a690fb3df2e16bd0a9a7cbb670b65ad17f81a08
Zachary Coats <zachary4coats@gmail.com>
2025-03-14 09:57:27 -0400

    Added env config on backend!

--- Backend(MySQL & Kotlin)/src/main/kotlin/Backend/streaming/Application.kt
+++ Backend(MySQL & Kotlin)/src/main/kotlin/Backend/streaming/Application.kt
@@ -1,5 +1,8 @@
 package Backend.streaming
 
+
+import io.github.cdimascio.dotenv.Dotenv
+import io.github.cdimascio.dotenv.dotenv
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -7,6 +10,14 @@ import org.springframework.boot.runApplication
 class Application
 
 fun main(args: Array<String>) {
+
+	val dotenv: Dotenv = dotenv()
+
+	// Optional: Print the environment variables for debugging purposes
+	dotenv.entries().forEach { entry ->
+		println("${entry.key} = ${entry.value}")
+		System.setProperty(entry.key, entry.value) // Optional: set system properties for debugging
+	}
 	runApplication<Application>(*args)
 
 
